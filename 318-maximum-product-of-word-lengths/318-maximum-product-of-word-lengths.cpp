526eddfab192664e9564e630552f75f328597ffd
aummishra <85782193+aummishra@users.noreply.github.com>
2022-06-05 14:09:57 +0530

    Time: 330 ms (21.87%), Space: 22 MB (16.22%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int maxProduct(vector<string>& words) {
         int n = words.size();
-        vector<int> mask(n,0);
-        int ans =0;
+        vector<bitset<26>> bs(n);
+        bool flag = false;
+        int ans = 0;
         for(int i=0;i<n;i++){
             for(auto it: words[i]){
-                mask[i] = mask[i]| 1<<(it-'a'); // shifting 1 character times
+                bs[i][it-'a'] = 1;
             }
-            for(int j=0;j<i;j++){ //Checking previous words to find words without any common character.
-                if((mask[j] & mask[i])==0){
-                    ans = max(ans,int(words[i].size()*words[j].size()));
+            for(int j=0;j<i;j++){
+                flag = false;
+                for(int k=0;k<26;k++){
+                    if(bs[i][k] && bs[j][k] == true){
+                        flag = true;
+                        break;
+                    }
                 }
+                if(flag == false)
+                ans = max(ans,int(words[i].size()*words[j].size()));
             }
         }
         return ans;
