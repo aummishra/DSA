a184794796e3138ed4dbf3dcd7b23c99efa48d91
aummishra <85782193+aummishra@users.noreply.github.com>
2022-06-12 17:57:57 +0530

    Time: 1006 ms (5.11%), Space: 156.8 MB (7.66%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        //sliding window
-        map<int,int> mp;
+        set<int> st;
         int ans = INT_MIN;
         int sum = 0;
         int j= 0;
         int i =0;
-        bool flag = true;
-        while(j<nums.size()){
-            if(flag){
-              mp[nums[j]]++;
-            sum += nums[j];  
-            }
-            if(mp.size()==j-i+1){
-                ans = max(sum,ans);
-                j++;
-                flag = true;
-            }
-            else if(mp.size()<j-i+1){
-               flag = false;
-                mp[nums[i]]--;
-                sum = sum - nums[i];
-                //ans = max(sum,ans);
-                if(mp[nums[i]]==0) mp.erase(nums[i]);
-                i++;
-            }
-        }
+      while(j<nums.size()){
+         while(st.find(nums[j])!=st.end()){
+              sum = sum -nums[i];
+             st.erase(nums[i]);
+             i++;
+          }
+          st.insert(nums[j]);
+          sum += nums[j];
+          ans = max(ans,sum);
+          j++;
+      }
         return ans;
     }
 };
