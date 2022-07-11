07c5bf0bed3bffd5653106b447dccafbc6636e65
aummishra <85782193+aummishra@users.noreply.github.com>
2022-07-11 12:43:40 +0530

    Time: 1879 ms (5.02%), Space: 51.4 MB (5.78%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> dp;
+   bool solve(vector<int>& nums,int i,int n){
+       if(i==n){
+           return true;
+       }
+       if(i>n){
+           return false;
+       }
+       if(dp[i]!=-1) return dp[i];
+       bool ans = false;
+       if(nums[i]==0) return false;
+       for(int j=1;j<=nums[i];++j){
+           if(ans==true) return dp[i]=true;
+           ans = (ans || solve(nums,i+j,n));
+       }
+       return dp[i] = ans;
+   }
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        dp.resize(n,-1);
+     return solve(nums,0,n-1);   
+    }
+};
