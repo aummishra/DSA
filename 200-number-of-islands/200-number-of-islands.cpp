5731a231e165809169c5a8c977133b02bf360e85
aummishra <85782193+aummishra@users.noreply.github.com>
2022-07-15 22:33:26 +0530

    Time: 86 ms (13.37%), Space: 12.3 MB (56.56%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int help(vector<vector<char>>& grid,int row,int col){
+        int n = grid.size();
+       int m = grid[0].size();
+        if((row<0 || row>=n) || (col<0 || col>=m)) return 0;
+        if(grid[row][col]=='0') return 0;
+        grid[row][col] = '0';
+        return 1 + help(grid,row+1,col) + help(grid,row-1,col) + help(grid,row,col+1) + help(grid,row,col-1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+         int n = grid.size();
+       int m = grid[0].size();
+        int cnt  = 0;
+       int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    ans = max(ans,help(grid,i,j));
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
