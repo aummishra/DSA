79d04c61b8cc90ee4a23f6e7dbc0a958d0c126a1
aummishra <85782193+aummishra@users.noreply.github.com>
2022-07-16 18:34:33 +0530

    Time: 2 ms (99.21%), Space: 6.4 MB (90.53%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int dp[51][51][51];
     int mod = 1e9+7;
-    long long help(int m, int n, int maxMove, int row, int col, vector<vector<int>> &grid){
+    long long help(int m, int n, int maxMove, int row, int col){
         if((row>=m ||  row<0) || (col>=n || col<0)){
             return 1;
         }
         if(maxMove == 0) return 0;
         if(dp[maxMove][row][col]!=-1) return dp[maxMove][row][col];
-        return dp[maxMove][row][col] = (help(m,n,maxMove-1,row+1,col,grid)%mod + help(m,n,maxMove-1,row-1,col,grid)%mod+ help(m,n,maxMove-1,row,col+1,grid)%mod + help(m,n,maxMove-1,row,col-1,grid)%mod)%mod;
+        return dp[maxMove][row][col] = (help(m,n,maxMove-1,row+1,col)%mod + help(m,n,maxMove-1,row-1,col)%mod+ help(m,n,maxMove-1,row,col+1)%mod + help(m,n,maxMove-1,row,col-1)%mod)%mod;
     }
         
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<int>> grid(m,vector<int>(n,0));
         memset(dp,-1,sizeof(dp));
-        return help(m,n,maxMove,startRow, startColumn,grid);
+        return help(m,n,maxMove,startRow, startColumn);
     }
 };
