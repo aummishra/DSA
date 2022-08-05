934b0281d7d7874bf88a2b89675a209783ebfb73
aummishra <85782193+aummishra@users.noreply.github.com>
2022-08-05 12:21:23 +0530

    Time: 11 ms (6.94%), Space: 10.9 MB (5.01%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int dp[202][1005];
+    int dp[1005][1005];
     Solution(){
         memset(dp,-1,sizeof(dp));
     }
-    int help(vector<int>& nums, int target,int n){
-        if(n==0 && target!=0) return 0;
-        if(target==0) return 1;
-        if(dp[n][target]!=-1) return dp[n][target];
-        if(nums[n-1]<=target){
-            return dp[n][target] = help(nums,target-nums[n-1],nums.size()) + help(nums,target,n-1);
-        }
-        else {
-            return dp[n][target] =  help(nums,target,n-1);
-        }
+    int solve(vector<int>& nums, int target,int n){
+       if(n==0 && target!=0){
+           return 0;
+       }
+      if(target==0) return 1;
+        if(dp[target][n]!=-1) return dp[target][n];
+      if(nums[n-1]<=target){
+          return dp[target][n]= solve(nums,target-nums[n-1],nums.size()) + solve(nums,target,n-1);
+      }
+        else
+            return dp[target][n] = solve(nums,target,n-1);  
     }
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
-        int ans = help(nums,target,n);
-        return ans;
+        return solve(nums,target,n);
     }
 };
