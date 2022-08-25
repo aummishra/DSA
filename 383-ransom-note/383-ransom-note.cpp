a1ea54fb1dd3f19df037aeb9883eedb9a4f48129
aummishra <85782193+aummishra@users.noreply.github.com>
2022-08-25 14:18:40 +0530

    Time: 73 ms (5.61%), Space: 9.1 MB (6.70%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int> mp1;
+        map<char,int> mp2;
+        for(auto it: magazine){
+            mp2[it]++;
+        }
+          for(auto it: ransomNote){
+            mp1[it]++;
+        }
+        for(auto it: mp1){
+            if(mp2.find(it.first) == mp2.end() || mp2[it.first]<it.second){
+                return false;
+            }
+        }
+        return true;
+    }
+};
